b0e461d697e6f30fdb3d538dc4b92e344391b0aa
Jose Valdvogel <valdvogel@hotmail.com>
2020-02-20 22:43:15 -0800

    twilio

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -8,63 +8,79 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-    constructor(
-        @InjectRepository(TaskRepository)
-        private taskRepository: TaskRepository) { }
-    // private tasks : Task[] = [];
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository) { }
+  // private tasks : Task[] = [];
 
-    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]>{
-        return this.taskRepository.getTasks(filterDto);
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
 
+  }
+
+  // getTasksWithFilters(filterDto: GetTasksFilterDto) : Task[] {
+  //     const {status, search} = filterDto;
+
+  //     let tasks = this.getAllTasks();
+
+  //     if(status){
+  //         tasks = tasks.filter(t => t.status === status);
+  //     }
+
+  //     if(search){
+  //         tasks = tasks.filter(t => t.title.includes(search) || t.description.includes(search));
+  //     }
+
+  //     return tasks;
+
+  // }
+  // getAllTasks(): Task[]{
+  //     return this.tasks;
+  // }
+  async getTaskById(id: number): Promise<Task> {
+    const found = await this.taskRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with id ${id} was not found!`);
+    }
+    return found;
+  }
+
+  async deleteTaskById(id: number): Promise<void> {
+    // const found = await this.getTaskById(id);
+    const result = await this.taskRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with id ${id} was not found!`);
     }
 
-    // getTasksWithFilters(filterDto: GetTasksFilterDto) : Task[] {
-    //     const {status, search} = filterDto;
 
-    //     let tasks = this.getAllTasks();
+  }
 
-    //     if(status){
-    //         tasks = tasks.filter(t => t.status === status);
-    //     }
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    try{
+    // const accountSid = 'AC807126ba4999f6fc308c1522bd2dcad7';
+    // const authToken = '189f9932ae8fc26a6c144521e99e0899';
+    // const client = require('twilio')(accountSid, authToken);
 
-    //     if(search){
-    //         tasks = tasks.filter(t => t.title.includes(search) || t.description.includes(search));
-    //     }
+    // client.messages
+    //   .create({
+    //     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+    //     from: '+18174947659',
+    //     to: '+16045377659'
+    //   })
+    //   .then(message => console.log(message.sid));
 
-    //     return tasks;
-
-    // }
-    // getAllTasks(): Task[]{
-    //     return this.tasks;
-    // }
-    async getTaskById(id: number): Promise<Task> {
-        const found = await this.taskRepository.findOne(id);
-
-        if (!found) {
-            throw new NotFoundException(`Task with id ${id} was not found!`);
-        }
-        return found;
+    return this.taskRepository.createTask(createTaskDto);
+    } catch(e){
+      console.log(e);
     }
+  }
 
-    async deleteTaskById(id: number): Promise<void> {
-        // const found = await this.getTaskById(id);
-        const result = await this.taskRepository.delete(id);
-        if (result.affected === 0) {
-            throw new NotFoundException(`Task with id ${id} was not found!`);
-        }
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    task.status = status;
 
-
-    }
-
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.taskRepository.createTask(createTaskDto);
-    }
-
-    async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-        const task = await this.getTaskById(id);
-        task.status = status;
-
-        await task.save();
-        return task;
-    }
+    await task.save();
+    return task;
+  }
 }
