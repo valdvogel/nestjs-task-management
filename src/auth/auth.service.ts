8bdb7cfc4762c56a1fefb10d9ef674eecd7255fe
Jose Valdvogel <valdvogel@hotmail.com>
2020-02-23 21:23:16 -0800

    add passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,25 +2,31 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt.payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private readonly userRepository: UserRepository,
+    private jwtService: JwtService
   ){}
 
   async signUp(authCredential: AuthCredentialDto): Promise<void> {
     return this.userRepository.signUp(authCredential);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string>{
+  async signIn(authCredentialDto: AuthCredentialDto): Promise<{accessToken: string}> {
     const username =  await this.userRepository.validatUserPassword(authCredentialDto);
 
     if(!username){
       throw new UnauthorizedException('Invalid Credentials!');
     }
 
-    return username;
+    const payload: JwtPayload = {username};
+    const accessToken = await this.jwtService.sign(payload);
+
+    return  { accessToken };
 
   }
 }
